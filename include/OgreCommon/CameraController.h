dc08ee0246a5d5616d91764e6b42acec4a98b7b9
Edward LEI <charming.lei72@qq.com>
2019-04-27 18:16:12 +0800

    reformat source

--- include/OgreCommon/CameraController.h
+++ include/OgreCommon/CameraController.h
@@ -7,33 +7,33 @@
 
 namespace Demo
 {
-    class CameraController
-    {
-        bool                mUseSceneNode;
+  class CameraController
+  {
+    bool                mUseSceneNode;
 
-        bool                mSpeedMofifier;
-        bool                mWASD[4];
-        bool                mSlideUpDown[2];
-        float               mCameraYaw;
-        float               mCameraPitch;
-        public: float       mCameraBaseSpeed;
-        public: float       mCameraSpeedBoost;
+    bool                mSpeedMofifier;
+    bool                mWASD[4];
+    bool                mSlideUpDown[2];
+    float               mCameraYaw;
+    float               mCameraPitch;
+    public: float       mCameraBaseSpeed;
+    public: float       mCameraSpeedBoost;
 
-    private:
-        GraphicsSystem      *mGraphicsSystem;
+  private:
+    GraphicsSystem      *mGraphicsSystem;
 
-    public:
-        CameraController( GraphicsSystem *graphicsSystem, bool useSceneNode=false );
+  public:
+    CameraController( GraphicsSystem *graphicsSystem, bool useSceneNode=false );
 
-        void update( float timeSinceLast );
+    void update( float timeSinceLast );
 
-        /// Returns true if we've handled the event
-        bool keyPressed( const SDL_KeyboardEvent &arg );
-        /// Returns true if we've handled the event
-        bool keyReleased( const SDL_KeyboardEvent &arg );
+    /// Returns true if we've handled the event
+    bool keyPressed( const SDL_KeyboardEvent &arg );
+    /// Returns true if we've handled the event
+    bool keyReleased( const SDL_KeyboardEvent &arg );
 
-        void mouseMoved( const SDL_Event &arg );
-    };
+    void mouseMoved( const SDL_Event &arg );
+  };
 }
 
 #endif
